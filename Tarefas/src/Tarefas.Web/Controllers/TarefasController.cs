6aa7c259b29c0283df985fbe8f21bb69881faae5
romulo-dev <romulosr2012@hotmail.com>
2023-01-25 15:01:01 -0300

    Criando a IActionResult Details

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -3,15 +3,27 @@ using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers;
     public class TarefasController : Controller{
+    public List<TarefaViewModel> listaDeTarefas {get; set;}
+    public TarefasController (){
+         listaDeTarefas = new List<TarefaViewModel>(){
+            new TarefaViewModel(){Id = 1, Titulo = "Escovar os dentes", Descricao="usar sensodine"},
+            new TarefaViewModel(){Id = 2, Titulo = "Escovar os dentes", Descricao="usar sensodine"},
+        };
+    }   
     public IActionResult Create(){
         return View();
     }
     public IActionResult Index(){
-        var listaDeTarefas = new List<TarefaViewModel>(){
-            new TarefaViewModel(){Id = 1, Titulo = "Escovar os dentes", Descricao="usar sensodine"},
-            new TarefaViewModel(){Id = 2, Titulo = "Escovar os dentes", Descricao="usar sensodine"},
-        };
+        // var listaDeTarefas = new List<TarefaViewModel>(){
+        //     new TarefaViewModel(){Id = 1, Titulo = "Escovar os dentes", Descricao="usar sensodine"},
+        //     new TarefaViewModel(){Id = 2, Titulo = "Escovar os dentes", Descricao="usar sensodine"},
+        // };
         return View(listaDeTarefas);
+    }
+
+    public IActionResult Details(int id){
+        var tarefa = listaDeTarefas.Find(tarefa => tarefa.Id == id);
+        return View(tarefa);
     }
 
 }
---
+++ info.md
@@ -0,0 +1,6 @@
+dotnet new globaljson
+dotnet new mvc
+dotnet sln add ./src/Tarefas.Web/Tarefas.Web.csproj
+
+1) Para rodar o dotnet em modo watch
+dotnet watch run
